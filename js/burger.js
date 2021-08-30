887dc2a8ecc81e3595d6f542470ddb7d1d278791
Dergelyova <dergelevakristina@gmail.com>
2021-08-30 17:56:22 +0300

    Fixed burger resize

--- js/burger.js
+++ js/burger.js
@@ -1,18 +1,31 @@
-const menu = document.querySelector('.menu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
+(function() {
+    const menu = document.querySelector('.menu');
+    const closeMenu = document.querySelector('.closeMenu');
+    const openMenu = document.querySelector('.openMenu');
 
 
-closeMenu.addEventListener('click', close);
-openMenu.addEventListener('click', show);
+    closeMenu.addEventListener('click', close);
+    openMenu.addEventListener('click', show);
 
-function show() {
-    menu.style.display = 'flex';
-}
+    function show() {
+        menu.style.display = 'flex';
+    }
 
-function close() {
-    menu.style.display = 'none';
-}
+    function close() {
+        menu.style.display = 'none';
+    }
+
+    function showBurgerControls() {
+        if (window.innerWidth < 768) {
+            menu.style.display = 'none';
+        } else {
+            menu.style.display = 'flex';
+        }
+    }
+
+    showBurgerControls();
+    window.addEventListener('resize', showBurgerControls);
+})();
 
 // $(window).resize(function() {
 //     if (window.innerWidth > 768) {
