de92fb9171d0e36ae360cb2e3c862b3d446dde79
Dergelyova <dergelevakristina@gmail.com>
2021-09-01 10:30:48 +0300

    Changed burger closing

--- js/burger.js
+++ js/burger.js
@@ -2,10 +2,16 @@
     const menu = document.querySelector('.menu');
     const closeMenu = document.querySelector('.closeMenu');
     const openMenu = document.querySelector('.openMenu');
+    const pointMenu = document.querySelectorAll('.menu>a');
 
 
     closeMenu.addEventListener('click', close);
     openMenu.addEventListener('click', show);
+
+
+    for (let i = 0; i < pointMenu.length; i++) {
+        pointMenu[i].addEventListener('click', close);
+    }
 
     function show() {
         menu.style.display = 'flex';
